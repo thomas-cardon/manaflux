73f910d8cf687f8c98d65ead869e1d10904fb1b0
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-08-21 19:34:57 +0200

    Added restart event

--- src/assets/js/custom-components/reset-settings.js
+++ src/assets/js/custom-components/reset-settings.js
@@ -2,6 +2,5 @@ module.exports = function(el) {
   log.log(2, '[UI] Settings reset asked.');
   Mana.store.clear();
   UI.status('UI', 'reset-settings-status');
-
-  el.click(() => ipcRenderer.send('restart'));
+  ipcRenderer.send('restart');
 };
