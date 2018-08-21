38b97ebc1930d4b6f81937670b7d02e34f8353e4
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-08-21 23:34:54 +0200

    Translation fixes
    Fixed an issue where some players using a language not yet translated had the UI hung.

--- objects/i18n/index.js
+++ objects/i18n/index.js
@@ -7,7 +7,7 @@ function i18n() {
     this._default = JSON.parse(fs.readFileSync(path.join(__dirname, '/locales/en.json'), 'utf8'));
 
     if(fs.existsSync(path.join(__dirname, '/locales/', this._locale + '.json')))
-      this._language = JSON.parse(fs.readFileSync(path.join(__dirname, '/locales/',  this._locale + '.json'), 'utf8')) || {};
+      this._language = JSON.parse(fs.readFileSync(path.join(__dirname, '/locales/',  this._locale + '.json'), 'utf8'));
   }
   catch(err) {
     log.error(0, err);
@@ -20,7 +20,7 @@ i18n.prototype.__d = function(...args) {
 }
 
 i18n.prototype.__ = function(...args) {
-  args[0] = this._language[args[0]] || this._default[args[0]] || args[0];
+  args[0] = this._language ? this._language[args[0]] : (this._default[args[0]] || args[0]);
   return args[0].includes("%s") ? require('util').format.call(this, ...args) : args[0];
 }
 
