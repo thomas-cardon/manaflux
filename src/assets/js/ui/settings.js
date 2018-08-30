31766c0fdb419d5a27ae6bb915ccfacbef01dc52
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-08-30 10:49:08 +0200

    Fixed checkboxes having the event fired twice
    - Fixed checkboxes having the event fired twice
    - Replaced some jQuery methods by native Javascript

--- src/assets/js/ui/settings.js
+++ src/assets/js/ui/settings.js
@@ -1,24 +1,21 @@
 /* input element support */
-$('input[data-settings-key], select[data-settings-key]').each(function() {
-  log.log(2, `[Settings] Loading value of ${$(this).data('settings-key')} to: ${Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default'))}`);
-  $(this).val(Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default')));
+$('input[type!="checkbox"][data-settings-key], select[data-settings-key]').each(function() {
+  log.log(2, `[Settings] Loading value of ${this.id = $(this).data('settings-key')} to: ${Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default'))}`);
+  this.value = Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default'));
 }).change(function() {
   log.log(2, `[Settings] Changing value of ${$(this).data('settings-key')} to: ${this.value}`);
   Mana.getStore().set($(this).data('settings-key'), this.value);
 });
 
 /* checkbox element support */
-$(':checkbox[data-settings-key]').each(function() {
-  log.log(2, `[Settings] Loading value of ${$(this).data('settings-key')} to: ${Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default'))}`);
-  $(this).prop('checked', Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default')));
+$('input[type="checkbox"]').each(function() {
+  log.log(2, `[Settings] Loading value of ${this.id = $(this).data('settings-key')} to: ${Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default'))}`);
 
-  $(this).prop('id', $(this).data('settings-key'));
+  this.checked = Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default'));
   $(this).siblings('label').prop('for', $(this).data('settings-key'));
-
-  $(this);
 }).change(function() {
   log.log(2, `[Settings] Changing value of ${$(this).data('settings-key')} to: ${$(this).is(":checked")}`);
-  Mana.getStore().set($(this).data('settings-key'), $(this).is(":checked"));
+  Mana.getStore().set($(this).data('settings-key'), this.checked);
 });
 
 /* sortable lists support */
