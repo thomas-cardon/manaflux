c2ec9e41dfa5240f4f07d6e2e4df585faef640e4
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-08-30 11:19:23 +0200

    Fixed gradient not enabling or disabling on checkbox change event

--- src/assets/js/custom-components/ui-gradient.js
+++ src/assets/js/custom-components/ui-gradient.js
@@ -3,7 +3,7 @@ $('body').css('background', `${Mana.getStore().get('ui-gradient') ? 'linear-grad
 
 module.exports = {
   change: function(el) {
-    log.log(2, `[UI] Changed gradient to ${this.value}`);
-    $('body').css('background', `${this.value ? 'linear-gradient(to bottom, rgba(125, 185, 232, 0) -1%, rgba(50, 96, 122, 0) 65%, rgba(10, 49, 64, 0.8) 100%), ' : ''}url(./assets/img/${Mana.getStore().get('ui-background')})`);
+    log.log(2, `[UI] Changed gradient to ${this.checked}`);
+    $('body').css('background', `${this.checked ? 'linear-gradient(to bottom, rgba(125, 185, 232, 0) -1%, rgba(50, 96, 122, 0) 65%, rgba(10, 49, 64, 0.8) 100%), ' : ''}url(./assets/img/${Mana.getStore().get('ui-background')})`);
   }
 };
