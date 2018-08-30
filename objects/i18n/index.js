25bd59cc67c8d65d03f243002ada9c412f0ba0b3
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-08-30 13:39:57 +0200

    Fixed translation module not showing default translations when lines were missing

--- objects/i18n/index.js
+++ objects/i18n/index.js
@@ -20,7 +20,7 @@ i18n.prototype.__d = function(...args) {
 }
 
 i18n.prototype.__ = function(...args) {
-  args[0] = (this._language ? this._language[args[0]] : this._default[args[0]]) || args[0];
+  args[0] = (this._language && this._language[args[0]] ? this._language[args[0]] : this._default[args[0]]) || args[0];
   return args[0].includes("%s") ? require('util').format.call(this, ...args) : args[0];
 }
 
