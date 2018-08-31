a2789f8ad9f12f8c8164734363d94e56c17d03a6
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-08-31 17:49:15 +0200

    Update module: removed channels
    Not working correctly, not even sure if it's supported by the dependency I'm using plus it will be awful for me to release even more versions.

--- src/assets/js/custom-components/update-channel.js
+++
@@ -1,9 +0,0 @@
-log.log(2, `[Update] Loaded channel: ${$('[data-settings-key="update-channel"]').val()}`);
-ipcRenderer.send('update-channel-change', $('[data-settings-key="update-channel"]').val());
-
-module.exports = {
-  change: function(el) {
-    log.log(2, `[Update] Changed to channel: ${this.value}`);
-    ipcRenderer.send('update-channel-change', this.value);
-  }
-};
