21402fb182b42881180c0d9f0723f69cf10e8906
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-09-12 21:44:17 +0200

    Fixed reset settings button

--- src/assets/js/custom-components/reset-settings.js
+++ src/assets/js/custom-components/reset-settings.js
@@ -1,6 +1,6 @@
 module.exports = function(el) {
   log.log(2, '[UI] Settings reset asked.');
-  Mana.store.clear();
+  Mana.getStore().clear();
   UI.status('UI', 'reset-settings-status');
   ipcRenderer.send('restart');
 };
