6cf2bea8c9854f840eed29b2f9d2755dd37f8459
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-09-18 22:29:16 +0200

    Wizard window now prevents the use of the main window as long as it's not closed

--- objects/Wizard.js
+++ objects/Wizard.js
@@ -1,7 +1,7 @@
 const { BrowserWindow } = require('electron').remote;
 
 module.exports = function(dev) {
-  let wiz = new BrowserWindow({ width: 700, height: 220, frame: false, icon: __dirname + '/build/icon.' + (process.platform === 'win32' ? 'ico' : 'png'), backgroundColor: '#000A13', maximizable: false, parent: require('electron').remote.getCurrentWindow(), show: false });
+  let wiz = new BrowserWindow({ width: 700, height: 220, frame: false, icon: __dirname + '/build/icon.' + (process.platform === 'win32' ? 'ico' : 'png'), backgroundColor: '#000A13', maximizable: false, parent: require('electron').remote.getCurrentWindow(), modal: true, show: false });
 
   wiz.loadURL(`file://${__dirname}/../src/wizard.html`);
   wiz.setMenu(null);
