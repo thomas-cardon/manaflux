33b4e1a6f041c220c3a1e8f96446535ea0e18840
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-09-22 13:43:08 +0200

    Fixed league-client-path not showing properly

--- src/assets/js/custom-components/league-client-path.js
+++ src/assets/js/custom-components/league-client-path.js
@@ -1,5 +1,5 @@
 module.exports = function() {
-  this.value = i18n.__('common-loading');
+  this.value = Mana.getStore().get('league-client-path') || i18n.__('common-loading');
   ipcRenderer.on('lcu-get-path', (event, path) => {
     log.log(2, `[UI] League path has changed to: ${path}`);
     Mana.getStore().set('league-client-path', this.value = path);
