b831c0c94748a2aac0177a0bd0a5fa58b4b37f5e
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-07 22:57:48 +0200

    Manaflux cache fixes

--- objects/providers/Manaflux.js
+++ objects/providers/Manaflux.js
@@ -11,9 +11,12 @@ class ManafluxProvider extends Provider {
     console.log(2, '[Manaflux] Fetching data from the cache server');
 
     let data = JSON.parse(await rp(`${this.base}v1/data/${champion.id}`));
+    if (Array.isArray(data)) data = data[0];
 
-    if (data && data[0]) return data[0];
-    else if (data) return data;
+    if (data) {
+      Object.keys(data.roles).forEach(r => data.roles[r].itemsets.map(x => require('./ItemSetHandler').parse(champion.key, x, r, 'manaflux')));
+      return data;
+    }
     else if (data.message) throw Error(`Manaflux cache server error: ${data.statusCode} - ${data.message} (${data.error})`);
     else throw Error('Unexpected error');
   }
@@ -36,16 +39,12 @@ class ManafluxProvider extends Provider {
    */
   async upload(data) {
     console.log(2, '[ProviderHandler] Uploading to Manaflux cache server');
-
-    const roles = Object.values(data.roles);
-    if (roles.some(x => x.summonerspells.length === 0 || x.itemsets.length === 0 || x.perks.length === 0)) return console.log(2, 'Upload cancelled: missing data');
-
-    roles.forEach(r => r.itemsets = r.itemsets.map(x => x.build()));
+    if (Object.values(data.roles).some(x => x.summonerspells.length === 0 || x.itemsets.length === 0 || x.perks.length === 0)) return console.log(2, 'Upload cancelled: missing data');
 
     await rp({
       method: 'POST',
       uri: `${this.base}v1/data`,
-      body: console.dir(3, JSON.stringify(data)),
+      body: console.dir(3, data),
       json: true
     });
   }
