8eb6e26ead8a26e157dec5bb6517076d43a169e2
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-12 21:40:45 +0200

    Update objects/leagueplug/tests.js

--- objects/leagueplug/tests.js
+++ objects/leagueplug/tests.js
@@ -1,10 +1,13 @@
 const LeaguePlug = require('./');
 const instance = new LeaguePlug();
 
+console.log('Loading LeaguePlug');
 instance.load().then(() => {
   instance.start();
-  instance.getConnectionHandler().on('connected', d => console.log('connected'));
-  instance.getConnectionHandler().on('logged-in', () => console.log('logged-in'));
+  console.log('Started LeaguePlug');
+
+  instance.getConnectionHandler().on('connected', d => console.log('connected', d));
+  instance.getConnectionHandler().on('logged-in', d => console.log('logged-in', d));
   instance.getConnectionHandler().on('logged-off', () => console.log('logged-off'));
   instance.getConnectionHandler().on('disconnected', () => console.log('disconnected'));
 });
