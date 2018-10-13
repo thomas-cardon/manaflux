af83420a394771f68a86fc4d5a7db62d0a175ff6
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-13 19:36:02 +0200

    Flu.x improvements

--- objects/providers/Flux.js
+++ objects/providers/Flux.js
@@ -4,21 +4,24 @@ const Provider = require('./Provider');
 class FluxProvider extends Provider {
   constructor() {
     super('flux', 'Flu.x');
-    this.base = 'http://localhost:8920/';
+    this.base = Mana.devMode ? 'http://localhost:8920/' : 'https://manaflux-server.herokuapp.com/';
   }
 
   async getData(champion, preferredPosition, gameMode) {
     console.log(2, '[Manaflux] Fetching data from the cache server');
 
     let data = JSON.parse(await rp(`${this.base}v1/data/${champion.id}`));
-    if (Array.isArray(data)) data = data[0];
+
+    console.dir(data);
 
     if (data.message) {
       if (data.statusCode === 404) throw Error(`Flu.x: Data not found`);
       else throw Error(`Flu.x error: ${data.statusCode} - ${data.message} (${data.error})`);
     }
     else if (data && data.roles) {
-      Object.keys(data.roles).filter(x => !x.startsWith('_')).forEach(r => data.roles[r].itemsets.map(x => require('../handlers/ItemSetHandler').parse(champion.key, x, r, this._id)));
+      Object.keys(data.roles).filter(x => !x.startsWith('_')).forEach(r => {
+        data.roles[r].itemsets = data.roles[r].itemsets.map(x => require('../handlers/ItemSetHandler').parse(champion.key, x, r, this._id));
+      });
       return data;
     }
     else throw Error('Unexpected error');
@@ -43,12 +46,17 @@ class FluxProvider extends Provider {
   async upload(data) {
     console.log(2, '[ProviderHandler] Uploading to Flu.x');
     if (Object.values(data.roles).some(x => Array.isArray(x) && x.length === 0)) return console.log(2, 'Upload cancelled: missing data');
-    Object.values(data.roles).forEach(r => r.itemsets.map(x => x.build()));
+
+    Object.values(data.roles).forEach(r => {
+      r.itemsets = r.itemsets.map(x => x.build(false));
+    });
+
+    console.dir(data);
 
     await rp({
       method: 'POST',
       uri: `${this.base}v1/data`,
-      body: console.dir(3, data),
+      body: data,
       json: true
     });
   }
