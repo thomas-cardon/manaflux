2db3b7bbd3390343fa49a81b5e0972a95c54fccd
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-16 12:41:22 +0200

    GameModeHandler improvements

--- objects/gameModesHandlers/ARAM.js
+++ objects/gameModesHandlers/ARAM.js
@@ -1,11 +1,15 @@
 const ClassicGameMode = require('./CLASSIC');
 class ARAMGameMode extends ClassicGameMode {
   constructor(ChampionSelectHandler, ProviderHandler) {
-    super(ChampionSelectHandler, ProviderHandler);
+    super(ChampionSelectHandler, ProviderHandler, 'ARAM');
   }
 
   getPosition() {
     return null;
+  }
+
+  getProviders() {
+    return [''];
   }
 }
 
