625dcc381791abc733ef8115ee8c5f58cf5312df
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-19 14:35:38 +0200

    Replaced navigation arrows by dots
    - jQuery isn't used anymore in tab-handler.js
    - Removed navigation arrows
    - Added dots
    - Replaced tabid attribute by tabId

--- src/assets/js/ui/tab-handler.js
+++ src/assets/js/ui/tab-handler.js
@@ -1,18 +1,49 @@
 /* Tab Handler */
+const Dots = {};
+
+Dots.onClickEvent = function(dot, tabId, i) {
+	console.log(`.tabcontent[data-tabId="${tabId}"][data-tabn="${i}"]`);
+
+	document.querySelectorAll('.tabcontent').forEach(x => x.style.display = 'none');
+	document.querySelector(`.tabcontent[data-tabId="${tabId}"][data-tabn="${i}"]`).style.display = 'block';
+
+	if (!dot) return;
+	if (Dots.lastSelected) Dots.lastSelected.classList.remove('selected');
+	dot.classList.add('selected');
+
+	Dots.lastSelected = dot;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	document.querySelectorAll('.btn.tab').forEach(tab => {
+		tab.addEventListener('click', event => {
+			document.querySelector('.tab.active').classList.remove('active');
+			event.target.classList.add('active');
+
+			const dotsNumber = document.querySelectorAll(`.tabcontent[data-tabId="${$(event.target).data('tabId')}"]`).length, dots = document.getElementById('dots');
+
+			if (dotsNumber === 0) Dots.onClickEvent(null, event.target.getAttribute('data-tabId'), 0);
+			else {
+				while (dots.firstChild) dots.removeChild(dots.firstChild);
+
+				if (dotsNumber > 1)
+					for (let i = 0; i < dotsNumber; i++) {
+						const dot = document.createElement('div');
+
+						dot.className = 'dot';
+						dot.onclick = () => Dots.onClickEvent(dot, $(event.target).data('tabId'), i);
+						dots.appendChild(dot);
+					}
+
+				Dots.onClickEvent(dots.firstChild, event.target.getAttribute('data-tabId'), 0);
+			}
+
+			document.getElementById('selected').style.marginLeft = ($(event.target).offset().left + ($(event.target).width() / 2)) + 'px';
+		});
+	});
+});
+
 $(document).ready(function() {
 	$('.btn.tab').click(function() {
-    navigationId = 0;
-
-		$('.tabcontent').hide();
-		$(`.tabcontent[data-tabid=${$(this).data('tabid')}][data-tabn=0]`).show();
-
-		$('.tab').removeClass('active');
-		$(this).addClass('active');
-
-		/* Navigation Menus */
-		if ($(`.tabcontent[data-tabid=${$(this).data('tabid')}][data-tabn=1]`).length) $('#nav-menu').show();
-		else $('#nav-menu').hide();
-
-		document.getElementById('selected').style.marginLeft = ($(this).offset().left + ($(this).width() / 2)) + 'px';
 	}).eq(0).click();
 });
