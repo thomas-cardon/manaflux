d7974b0f247bd3a1584d3c71c1e90963311265a7
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-21 19:25:14 +0200

    ProviderHandler: fixed undefined data from cache

--- objects/handlers/ProviderHandler.js
+++ objects/handlers/ProviderHandler.js
@@ -17,7 +17,7 @@ class ProviderHandler {
     const gameMode = gameModeHandler.getGameMode() || 'CLASSIC';
 
     /* 1/4 - Storage Checking */
-    if (Mana.getStore().has(`data.${champion.id}`) && cache) return Mana.getStore().get(`data.${champion.key}`);
+    if (Mana.getStore().has(`data.${champion.id}`) && cache) return Mana.getStore().get(`data.${champion.id}`);
 
     /* 2/4 - Downloading */
     const providers = Mana.getStore().get('providers-order', Object.keys(this.providers)).filter(x => gameModeHandler.getProviders() === null || gameModeHandler.getProviders().includes(x));
@@ -34,6 +34,9 @@ class ProviderHandler {
         try {
           const x = await provider.getData(champion, preferredPosition, gameMode);
           data = { roles: {}, championId: champion.id, gameMode, version: Mana.version, gameVersion: Mana.gameClient.branch, region: Mana.gameClient.region, ...x };
+
+          console.dir(x);
+          console.dir(data);
         }
           catch(err) {
           console.error(err);
