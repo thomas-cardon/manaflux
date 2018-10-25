4ee3ea9b28d8af37d5636c53d0b61e5d5cd1cc1e
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-25 15:21:44 +0200

    Custom component: perks-max improvements
    Now prevents change events (e.g keyboard) as well

--- src/assets/js/custom-components/perks-max.js
+++ src/assets/js/custom-components/perks-max.js
@@ -1,11 +1,21 @@
 module.exports = {
   userConnected: async function(e) {
     this.max = await Mana.user.getPerksInventory().getCount() || 2;
-    this.value = this.value > this.max ? Mana.getStore().set('perks-max', this.max) : this.value;
+
+    if (this.value > this.max) {
+      Mana.getStore().set('perks-max', this.max)
+      this.value = this.max;
+    }
 
     this.disabled = false;
   },
   userDisconnected: function(e) {
     this.disabled = true;
+  },
+  change: function(e) {
+    if (this.value > this.max) {
+      Mana.getStore().set('perks-max', this.max)
+      this.value = this.max;
+    }
   }
 };
