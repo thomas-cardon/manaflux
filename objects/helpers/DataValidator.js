b505ebd065e047f2e473d60aec5a55eaeff73c10
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-26 14:49:53 +0200

    DataValidator: ItemSet fixes

--- objects/helpers/DataValidator.js
+++ objects/helpers/DataValidator.js
@@ -12,14 +12,18 @@ class DataValidator {
       });
 
       role.itemsets.forEach(x => {
-        if (!x.provider) x.provider = x._data.provider = providerId;
+        if (!x._data.provider) x._data.provider = providerId;
       });
     });
   }
 
-  onDataDownloaded(data, championId, gameMode) {
+  onDataDownloaded(d, champion, gameMode) {
+    if (!d) return null;
+
     console.log('[DataValidator] Copying required properties for Flu.x');
-    data.championId = championId;
+    let data = { ...d };
+
+    data.championId = champion.id;
 
     data.gameMode = gameMode;
     data.gameVersion = Mana.gameClient.branch;
@@ -28,36 +32,41 @@ class DataValidator {
     data.region = Mana.gameClient.region;
 
     for (const [roleName, role] of Object.entries(data.roles)) {
-      this.onPerkPagesCheck(role.perks, championId, roleName);
-      this.onItemSetsCheck(role.itemsets, championId, roleName);
+      role.perks = this.onPerkPagesCheck(role.perks, champion, roleName);
+      role.itemsets = this.onItemSetsCheck(role.itemsets, champion, roleName);
     }
+
+    return data;
   }
 
   onDataUpload(data) {
     for (const [roleName, role] of Object.entries(data.roles)) {
-      role.itemsets = role.itemsets.map(x => x._data ? x : ItemSetHandler.parse(champion.key, x, Mana.providerHandler.getProvider(x.provider).getCondensedName()));
-
       role.perks.forEach(x => delete x.name);
       role.itemsets.forEach(x => delete x._data.title);
     }
   }
 
-  onPerkPagesCheck(array, championId, role) {
+  onPerkPagesCheck(array, champion, role) {
     array = array.filter(x => x.selectedPerkIds && x.selectedPerkIds.length >= 6 && !this._hasDuplicates(x.selectedPerkIds));
 
     array.forEach((page, index) => { /* Recreates primaryStyleId or subStyleId based on perks if it's missing */
-      page.name = `${page.provider ? Mana.providerHandler.getProvider(page.provider).getCondensedName() : 'XXX'}${index + 1} ${Mana.champions[championId].name} > ${UI.stylizeRole(role)}${page.suffixName ? ' ' + page.suffixName : ''}`;
+      page.name = `${page.provider ? Mana.providerHandler.getProvider(page.provider).getCondensedName() : 'XXX'}${index + 1} ${champion.name} > ${UI.stylizeRole(role)}${page.suffixName ? ' ' + page.suffixName : ''}`;
 
       page.primaryStyleId = page.primaryStyleId || Mana.gameClient.findPerkStyleByPerkId(page.selectedPerkIds[0]).id;
       page.subStyleId = page.subStyleId || Mana.gameClient.findPerkStyleByPerkId(page.selectedPerkIds[4]).id;
     });
+
+    return array;
   }
 
-  onItemSetsCheck(array, championId, role) {
-    array = array.map((x, index) => {
-      if (!x._data) x = ItemSetHandler.parse(champion.key, x, Mana.providerHandler.getProvider(x.provider).getCondensedName());
-      x._data.title = `${x._data.provider ? Mana.providerHandler.getProvider(x._data.provider).getCondensedName() : 'XXX'}${index + 1} ${Mana.champions[championId].name} > ${UI.stylizeRole(role)}`;
+  onItemSetsCheck(array, champion, role) {
+    let indexes = {};
 
+    return array.map((x, index) => {
+      if (!x._data) x = ItemSetHandler.parse(champion.key, x, Mana.providerHandler.getProvider(x.provider).getCondensedName());
+      indexes[x._data.provider || 'XXX'] = indexes[x._data.provider || 'XXX'] + 1 || 1;
+
+      x._data.title = `${x._data.provider ? Mana.providerHandler.getProvider(x._data.provider).getCondensedName() : 'XXX'}${indexes[x._data.provider || 'XXX']} ${champion.name} > ${UI.stylizeRole(role)}`;
       return x;
     });
   }
