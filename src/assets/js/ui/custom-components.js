a8f44bf121bf3df81c3bf072e3220cec138bb0d1
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-10-28 00:47:18 +0200

    Custom components: added possibility to call method with onload attribute

--- src/assets/js/ui/custom-components.js
+++ src/assets/js/ui/custom-components.js
@@ -1,5 +1,4 @@
-/* Custom components */
-for (const el of document.querySelectorAll('[data-custom-component]')) {
+UI.loadCustomComponent = function(el) {
   console.log(2, `[UI] [Custom Components] Loading ${el.dataset.customComponent}`);
 
   const d = require(__dirname + '\\assets\\js\\custom-components\\' + el.dataset.customComponent + '.js');
@@ -9,4 +8,8 @@ for (const el of document.querySelectorAll('[data-custom-component]')) {
       if (ev !== 'metadata') d.metadata && d.metadata.includes('jQueryOnly') ? $(el).on(ev, f) : el.addEventListener(ev, f);
   }
   else d.apply(el);
+}
+
+for (const el of document.querySelectorAll('[data-custom-component]')) {
+  UI.loadCustomComponent(el);
 }
