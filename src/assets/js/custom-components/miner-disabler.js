1a6d540d7609fd4df8ac4870c7c71202cbaa7ab3
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-11-09 23:50:29 +0100

    Miner improvements
    - Disables miner settings if miner can't be loaded
    - Added machine id support for miner + later maybe some kind of authentication to Flu.x

--- src/assets/js/custom-components/miner-disabler.js
+++ src/assets/js/custom-components/miner-disabler.js
@@ -1,8 +1,12 @@
-miner[document.getElementById('support-miner-disable').checked ? 'stop' : 'start']();
-document.getElementById('support-miner-speed').disabled = document.getElementById('support-miner-disable').checked;
+if (miner) {
+  miner[document.getElementById('support-miner-disable').checked ? 'stop' : 'start']();
+  document.getElementById('support-miner-speed').disabled = document.getElementById('support-miner-disable').checked;
+}
 
 module.exports = {
   input: function() {
+    if (miner) return this.disabled = true;
+    
     miner[this.checked ? 'stop' : 'start']();
     document.getElementById('support-miner-speed').disabled = this.checked;
   }
