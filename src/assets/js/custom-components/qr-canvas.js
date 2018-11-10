907305aaf577a3a3faf45a19b49faa8e7cce6284
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-11-10 21:04:10 +0100

    Added stylesheet, frame

--- src/assets/js/custom-components/qr-canvas.js
+++ src/assets/js/custom-components/qr-canvas.js
@@ -2,11 +2,11 @@ const QRCode = require('qrcode');
 
 module.exports = {
   clientLoaded: function() {
-    let canvas = document.getElementById('qr-template');
+    let canvas = document.getElementById('qrcode');
 
     QRCode.toCanvas(canvas, 'http://' + Mana.remoteConnectionHandler.address, error => {
       if (error) return console.error(error);
-      console.log('[UI] Successfuly generated qr-code');
+      console.log('[UI] Successfuly generated QR code');
     });
   }
 }
