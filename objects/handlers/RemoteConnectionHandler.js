e3f7f420cc4e6a0a18a61ffc56372fdcb16ee9ec
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-11-10 21:59:56 +0100

    Implemented basic server

--- objects/handlers/RemoteConnectionHandler.js
+++ objects/handlers/RemoteConnectionHandler.js
@@ -1,11 +1,30 @@
+const polka = require('polka');
 const os = require('os');
+
 class RemoteConnectionHandler {
   constructor() {
     this.address = this._queryAddress();
   }
 
   async start() {
+    function log(req, res, next) {
+      console.log(`[RemoteConnectionHandler] > ${req.url}`);
+      next();
+    }
 
+    this._server = polka()
+      .use(log)
+      .get('/heartbeat', (req, res) => {
+        res.end('true');
+      })
+      .get('/summoner', (req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ }));
+      })
+      .listen(4500, err => {
+        if (err) throw err;
+        console.log(`[RemoteConnectionHandler] > Running on localhost:4500`);
+      });
   }
 
   async stop() {
