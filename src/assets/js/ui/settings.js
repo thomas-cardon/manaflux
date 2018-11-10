fa12459b1c5ae30dfbb117d017ef0e21132c96ac
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-11-10 12:50:02 +0100

    Settings: replaced jQuery by native javascript for input elements/checkboxes

--- src/assets/js/ui/settings.js
+++ src/assets/js/ui/settings.js
@@ -1,41 +1,42 @@
 /* input element support */
-$('input[type!="checkbox"][data-settings-key], select[data-settings-key]').each(function() {
-  console.log(2, `[Settings] Loading value of ${this.id = $(this).data('settings-key')} to: ${Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default'))}`);
+document.querySelectorAll('input:not([type="checkbox"])[data-settings-key], select[data-settings-key]').forEach(function(el) {
+  console.log(2, `[Settings] Loading value of ${el.id = el.dataset.settingsKey} to: ${Mana.getStore().get(el.dataset.settingsKey, el.dataset.settingsDefault)}`);
 
-  if (!Mana.getStore().has($(this).data('settings-key')) && $(this).data('settings-default'))
-    Mana.getStore().set($(this).data('settings-key'), $(this).data('settings-default'));
+  if (!Mana.getStore().has(el.dataset.settingsKey) && el.dataset.settingsDefault)
+    Mana.getStore().set(el.dataset.settingsKey, el.dataset.settingsDefault);
 
-  this.value = Mana.getStore().get($(this).data('settings-key'));
-  this.addEventListener('input', function() {
-    console.log(2, `[Settings] Changing value of ${$(this).data('settings-key')} to: ${this.value}`);
-    Mana.getStore().set($(this).data('settings-key'), this.value);
+  el.value = Mana.getStore().get(el.dataset.settingsKey);
+  el.addEventListener('input', function() {
+    console.log(2, `[Settings] Changing value of ${el.dataset.settingsKey} to: ${el.value}`);
+    Mana.getStore().set(el.dataset.settingsKey, el.value);
 
     Sounds.play('dropdownSelect');
   });
 });
 
 /* checkbox element support */
-$('input[type="checkbox"]').each(function() {
-  console.log(2, `[Settings] Loading value of ${this.id = $(this).data('settings-key')} to: ${Mana.getStore().get($(this).data('settings-key'), $(this).data('settings-default'))}`);
+document.querySelectorAll('input[type="checkbox"][data-settings-key]').forEach(function(el) {
+  console.log(2, `[Settings] Loading value of ${el.id = el.dataset.settingsKey} to: ${Mana.getStore().get(el.dataset.settingsKey, el.dataset.settingsDefault)}`);
 
-  if (!Mana.getStore().has($(this).data('settings-key')) && $(this).data('settings-default'))
-    Mana.getStore().set($(this).data('settings-key'), $(this).data('settings-default'));
+  if (!Mana.getStore().has(el.dataset.settingsKey) && el.dataset.settingsDefault)
+    Mana.getStore().set(el.dataset.settingsKey, el.dataset.settingsDefault);
 
-  this.checked = Mana.getStore().get($(this).data('settings-key'));
-  $(this).siblings('label').prop('for', $(this).data('settings-key'));
-}).change(function() {
-  console.log(2, `[Settings] Changing value of ${$(this).data('settings-key')} to: ${$(this).is(":checked")}`);
-  Mana.getStore().set($(this).data('settings-key'), this.checked);
+  el.checked = Mana.getStore().get(el.dataset.settingsKey);
+  $(el).siblings('label').prop('for', el.dataset.settingsKey);
+  el.addEventListener('input', function() {
+    console.log(2, `[Settings] Changing value of ${el.dataset.settingsKey} to: ${$(el).is(":checked")}`);
+    Mana.getStore().set(el.dataset.settingsKey, el.checked);
 
-  Sounds.play('checkboxClick');
+    Sounds.play('checkboxClick');
+  });
 });
 
 /* sortable lists support */
 $(".sortable[data-settings-key]").each(function() {
   const mergeIfMissing = $(this).data('settings-merge-list-if-missing-value') || false, removeIfMissing = $(this).data('settings-remove-if-missing-value') || false;
 
-  const defaultValues = $(this).data('settings-default').split(',');
-  const values = Mana.getStore().get($(this).data('settings-key'), defaultValues);
+  const defaultValues = this.dataset.settingsDefault.split(',');
+  const values = Mana.getStore().get(this.dataset.settingsKey, defaultValues);
 
   if (mergeIfMissing) {
     let saveNeeded;
@@ -46,7 +47,7 @@ $(".sortable[data-settings-key]").each(function() {
       }
     }
 
-    if (saveNeeded) Mana.getStore().set($(this).data('settings-key'), values);
+    if (saveNeeded) Mana.getStore().set(this.dataset.settingsKey, values);
   }
 
   if (removeIfMissing) {
@@ -58,11 +59,11 @@ $(".sortable[data-settings-key]").each(function() {
       }
     }
 
-    if (saveNeeded) Mana.getStore().set($(this).data('settings-key'), values);
+    if (saveNeeded) Mana.getStore().set(this.dataset.settingsKey, values);
   }
 
   for (let i = 0; i < values.length; i++)
-    $(this).append(`<li class="ui-state-default sortable-button" value="${values[i]}">${i18n.__('settings-' + $(this).data('settings-key') + '-' + values[i])}</li>`);
+    $(this).append(`<li class="ui-state-default sortable-button" value="${values[i]}">${i18n.__('settings-' + this.dataset.settingsKey + '-' + values[i])}</li>`);
 
   $(this).css('list-style', 'disc inside').sortable({
     update: function(event, ui) {
@@ -71,8 +72,8 @@ $(".sortable[data-settings-key]").each(function() {
         array.push($(this).attr('value'));
       });
 
-      Mana.getStore().set($(this).data('settings-key'), array);
-      console.log(2, `[Settings] Changing value of #${$(this).data('settings-key')} to: ${array}`);
+      Mana.getStore().set(this.dataset.settingsKey, array);
+      console.log(2, `[Settings] Changing value of #${this.dataset.settingsKey} to: ${array}`);
 
       Sounds.play('checkboxClick');
     }
