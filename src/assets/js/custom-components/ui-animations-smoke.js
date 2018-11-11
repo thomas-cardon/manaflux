36ccbc64b8fe954d7d4ea06179124861f5a71691
Ryzzzen <Ryzzzen@users.noreply.github.com>
2018-11-11 15:37:20 +0100

    Custom components update

--- src/assets/js/custom-components/ui-animations-smoke.js
+++ src/assets/js/custom-components/ui-animations-smoke.js
@@ -1,11 +1,12 @@
-if (Mana.getStore().get('ui-animations-smoke')) {
-  document.getElementById('smokeAnimation').play();
-  document.getElementById('smokeAnimation').style.display = 'block';
-}
-else document.getElementById('smokeAnimation').style.display = 'none';
-
 module.exports = {
-  change: function(el) {
+  load: function(Mana) {
+    if (Mana.getStore().get('ui-animations-smoke')) {
+      document.getElementById('smokeAnimation').play();
+      document.getElementById('smokeAnimation').style.display = 'block';
+    }
+    else document.getElementById('smokeAnimation').style.display = 'none';
+  },
+  change: function() {
     document.getElementById('smokeAnimation').style.display = this.checked ? 'block' : 'none';
 
     if (this.checked) document.getElementById('smokeAnimation').play();
