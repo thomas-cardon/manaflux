46022b8090fe6758767129146ef493895ca32d36
kko7 <j4kub.kko7@gmail.com>
2018-12-29 07:42:39 +0100

    hostname in qrcode

--- src/assets/js/custom-components/qr-canvas.js
+++ src/assets/js/custom-components/qr-canvas.js
@@ -4,7 +4,7 @@ module.exports = {
   clientLoaded: function() {
     let canvas = document.getElementById('qrcode');
 
-    QRCode.toCanvas(canvas, Mana.remoteConnectionHandler.address, { width: 256 }, error => {
+    QRCode.toCanvas(canvas, os.hostname() + ":" + Mana.remoteConnectionHandler.address, { width: 256 }, error => {
       if (error) return console.error(error);
       console.log('[UI] Successfuly generated QR code');
     });
