f294d3620dfa4a3e9e88559a70da8e1a6e095abc
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-02-09 15:25:27 +0100

    Update ChampionSelectInterface.js

--- src/themes/default/assets/js/interfaces/ChampionSelectInterface.js
+++ src/themes/default/assets/js/interfaces/ChampionSelectInterface.js
@@ -1,4 +1,59 @@
 class ChampionSelectInterface {
+
+  onChampionSelectStart() {
+
+  }
+
+  onChampionNotPicked() {
+    return UI.status('champion-select-pick-a-champion');
+  }
+
+  onChampionChange(champion) {
+    document.getElementById('buttons').style.display = 'none';
+    while (document.getElementById('positions').firstChild) {
+      document.getElementById('positions').removeChild(document.getElementById('positions').firstChild);
+    }
+
+    $('#loadRunes, #loadSummonerSpells').disableManualButton(true);
+
+    UI.enableHextechAnimation(champion);
+    document.querySelector('button[data-tabid]').click();
+  }
+
+  onChampionDataDownloaded(champion, positions, data) {
+    let positionsHTML = '';
+    positions.forEach(r => {
+      console.log('[ChampionSelect] Added position:', r);
+      positionsHTML += `<option value="${r}">${UI.stylizeRole(r)}</option>`;
+    });
+
+    document.getElementById('positions').innerHTML = positionsHTML;
+    document.getElementById('positions').onchange = function() {
+      self.onPositionChange(champion, this.value.toUpperCase(), data[this.value.toUpperCase()]);
+    };
+
+    // Sets value and checks if it's not null, if it is then let's stop everything
+    if (!(document.getElementById('positions').value = res.roles[this.getPosition()] ? this.gameModeHandler.getPosition(this.getPosition()) : Object.keys(res.roles).filter(x => res.roles[x].perks.length > 0)[0])) {
+      Mana.championStorageHandler.remove(champion.id);
+      throw this._onCrash(i18n.__('champion-select-error-empty'));
+    }
+
+    document.getElementById('positions').onchange();
+
+    document.getElementById('positions').style.display = 'unset';
+    document.getElementById('buttons').style.display = 'block';
+  }
+
+  onPositionChange(position) {
+    console.log('Champion Select >> Position changed:', position);
+
+    document.getElementById('positions').value = position;
+    document.getElementById('positions').onchange();
+  }
+
+  onChampionSelectEnd() {
+
+  }
 }
 
-module.exports = ChampionSelectInterface;
+module.exports = new ChampionSelectInterface();
