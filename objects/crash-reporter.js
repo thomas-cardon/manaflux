4ce705571fd5dc2b88a6207b487ba20cc8b57ebc
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-03-10 01:07:00 +0100

    Bug reports, Downloader, Language tester, Wizard => UI fixes

--- objects/crash-reporter.js
+++ objects/crash-reporter.js
@@ -8,20 +8,23 @@ crashReporter.start({
 });
 
 /* Client-only method */
-if (!process || typeof process === 'undefined' || !process.type || process.type === 'renderer') {
-  ipcRenderer.on('lcu-logged-in', data => {
-    if (!data) return;
-    console.log('Crash Reporter >> Restarted because user logged in');
+try {
+  if (!process || typeof process === 'undefined' || !process.type || process.type === 'renderer') {
+    ipcRenderer.on('lcu-logged-in', data => {
+      if (!data) return;
+      console.log('Crash Reporter >> Restarted because user logged in');
 
-    crashReporter.start({
-      productName: 'Manaflux',
-      companyName: 'Ryzzzen/manaflux',
-      submitURL: 'http://localhost:8920/api/app-crashes/v1/manaflux',
-      uploadToServer: true,
-      extra: {
-        summonerId: data.summonerId,
-        summonerName: data.displayName
-      }
+      crashReporter.start({
+        productName: 'Manaflux',
+        companyName: 'Ryzzzen/manaflux',
+        submitURL: 'http://localhost:8920/api/app-crashes/v1/manaflux',
+        uploadToServer: true,
+        extra: {
+          summonerId: data.summonerId,
+          summonerName: data.displayName
+        }
+      });
     });
-  });
+  }
 }
+catch(err) {}
