5960ce7b6d80a68ab39c20529855c5a6b43181e2
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-05-24 21:44:11 +0200

    Electron 5 ready: wizard window

--- objects/Wizard.js
+++ objects/Wizard.js
@@ -1,10 +1,15 @@
 const { BrowserWindow } = require('electron').remote;
 
 module.exports = function(dev) {
-  let wiz = new BrowserWindow({ parent: require('electron').remote.getCurrentWindow(), width: 700, height: 220, frame: false, icon: __dirname + '/build/icon.' + (process.platform === 'win32' ? 'ico' : 'png'), backgroundColor: '#000A13', maximizable: false, resizable: false, modal: true, show: false });
+  let wiz = new BrowserWindow({ parent: require('electron').remote.getCurrentWindow(), width: 700, height: 220, frame: false, icon: __dirname + '/build/icon.' + (process.platform === 'win32' ? 'ico' : 'png'), backgroundColor: '#000A13', maximizable: false, resizable: false, modal: true, show: false, webPreferences: { nodeIntegration: true } });
 
-  wiz.loadURL(`file://${__dirname}/../src/wizard.html`);
-  wiz.setMenu(null);
+  win.loadURL(require('url').format({
+    pathname: require('path').join(__dirname, '/../src/wizard.html'), // important
+    protocol: 'file:',
+    slashes: true,
+  }));
+
+  wiz.removeMenu();
 
   wiz.once('ready-to-show', () => wiz.show());
 
