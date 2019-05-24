b1c5d79acf9a99619777783eba00b0cf2a56b37c
Ryzzzen <Ryzzzen@users.noreply.github.com>
2019-05-24 21:53:51 +0200

    Wizard fixes

--- objects/Wizard.js
+++ objects/Wizard.js
@@ -3,7 +3,7 @@ const { BrowserWindow } = require('electron').remote;
 module.exports = function(dev) {
   let wiz = new BrowserWindow({ parent: require('electron').remote.getCurrentWindow(), width: 700, height: 220, frame: false, icon: __dirname + '/build/icon.' + (process.platform === 'win32' ? 'ico' : 'png'), backgroundColor: '#000A13', maximizable: false, resizable: false, modal: true, show: false, webPreferences: { nodeIntegration: true } });
 
-  win.loadURL(require('url').format({
+  wiz.loadURL(require('url').format({
     pathname: require('path').join(__dirname, '/../src/wizard.html'), // important
     protocol: 'file:',
     slashes: true,
