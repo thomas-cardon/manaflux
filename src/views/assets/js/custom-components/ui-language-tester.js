1f2dbcc30d7f8816428491e8c6f2b0fad40a019a
Thomas <Ryzzzen@users.noreply.github.com>
2019-08-18 23:29:38 +0200

    Language tester works again

--- src/views/assets/js/custom-components/ui-language-tester.js
+++ src/views/assets/js/custom-components/ui-language-tester.js
@@ -2,7 +2,7 @@ const { BrowserWindow } = require('electron').remote;
 
 module.exports = {
   click: function() {
-    const win = new BrowserWindow({ parent: require('electron').remote.getCurrentWindow(), width: 350, height: 550, frame: false, icon: __dirname + '/build/icon.' + (process.platform === 'win32' ? 'ico' : 'png'), backgroundColor: '#000A13', maximizable: false, resizable: false, modal: true, show: false });
+    const win = new BrowserWindow({ parent: require('electron').remote.getCurrentWindow(), width: 350, height: 550, frame: false, icon: __dirname + '/build/icon.' + (process.platform === 'win32' ? 'ico' : 'png'), backgroundColor: '#000A13', maximizable: false, resizable: false, modal: true, show: false, webPreferences: { nodeIntegration: true } });
 
     win.loadURL(`file://${__dirname}/../../../languagetester.html`);
     win.setMenu(null);
