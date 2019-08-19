cf1075bddace495862c3afa16f8eae7019e63966
Thomas <Ryzzzen@users.noreply.github.com>
2019-08-19 13:47:43 +0200

    Revamped Manaflux to add a sidebar that can be turned on or off

---
+++ src/views/assets/js/custom-components/runes-list.js
@@ -0,0 +1,25 @@
+let Sidebar = {
+  off: function() {
+    remote.getCurrentWindow().setContentSize(remote.getCurrentWindow().getContentSize()[0] - 200, remote.getCurrentWindow().getContentSize()[1]);
+    document.getElementById('sidebar').style.display = 'none';
+    document.getElementById('window').style.left = '0px';
+    document.getElementById('footer').style.left = '20px';
+  },
+  on: function() {
+    remote.getCurrentWindow().setContentSize(remote.getCurrentWindow().getContentSize()[0] + 200, remote.getCurrentWindow().getContentSize()[1]);
+    document.getElementById('sidebar').style.display = 'block';
+    document.getElementById('window').style.left = '200px';
+    document.getElementById('footer').style.left = '220px';
+  },
+  reset: function() {
+    if (remote.getCurrentWindow().getContentSize()[0] >= 800)
+      remote.getCurrentWindow().setContentSize(600, remote.getCurrentWindow().getContentSize()[1]);
+  }
+};
+
+module.exports = function(Mana) {
+  UI.sidebar = Sidebar;
+
+  Sidebar.reset();
+  Sidebar.on();
+}
