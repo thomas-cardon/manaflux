b47a3759c4323e39e6bfbefd4a94fe92009a997e
Thomas <Ryzzzen@users.noreply.github.com>
2019-08-20 16:01:02 +0200

    Moved context menu to RunesInventory because it makes more sense

--- src/views/assets/js/custom-components/runes-inventory.js
+++ src/views/assets/js/custom-components/runes-inventory.js
@@ -1,8 +1,52 @@
-let timer = 0;
+const { Menu, MenuItem } = remote;
+const Runes = {
+  delete: async () => {
+    if (!document.getElementById(Runes._selected)) return console.error('Runes >> Context Menu: selected item doesn\'t exist !');
 
+    try {
+      await Mana.user.getPerksInventory().deletePerkPage(Runes._selected.split('-')[1]);
+      document.getElementById(Runes._selected).remove();
+    }
+    catch(err) {
+      console.error('Runes >> Context Menu: couldn\'t delete rune page !');
+      console.error(err);
+    }
+  },
+  select: async () => {
+    if (!document.getElementById(Runes._selected)) return console.error('Runes >> Context Menu: selected item doesn\'t exist !');
+
+    try {
+      await Mana.user.getPerksInventory().setCurrentPage(Runes._selected.split('-')[1]);
+      menu.getMenuItemById('checked') = true;
+    }
+    catch(err) {
+      console.error('Runes >> Context Menu: couldn\'t select rune page !');
+      console.error(err);
+    }
+  }
+};
+
+const menu = new Menu();
+menu.append(new MenuItem({ label: i18n.__('ui-sidebar-runes-context-delete'), id: 'delete', click: Runes.delete }));
+menu.append(new MenuItem({ type: 'separator' }));
+menu.append(new MenuItem({ label: i18n.__('ui-sidebar-runes-context-select'), id: 'select', type: 'checkbox', checked: true, click: Runes.select }));
+
+window.addEventListener('contextmenu', (e) => {
+  e.preventDefault();
+
+  if (e.target.id.startsWith('runes-')) {
+    Runes._selected = e.target.id;
+
+    menu.getMenuItemById('select').checked = true;
+    menu.popup({ window: remote.getCurrentWindow() });
+  }
+}, false);
+
+let timer = 0;
 module.exports = {
   load: function(Mana) {
     $('#runesInventory').sortable({
+      connectWith: '#availableRunes',
       update: function( event, ui ) {
         console.dir(arguments);
       }
