aa7562aa56adc412de8e2728daef3395f27791ec
Thomas <Ryzzzen@users.noreply.github.com>
2019-08-23 22:56:56 +0200

    Available Runes fixes, refactoring and UI improvements

--- src/views/assets/js/custom-components/runes-available.js
+++ src/views/assets/js/custom-components/runes-available.js
@@ -1,24 +1,33 @@
 const { Menu, MenuItem } = remote;
-
 const AvailableRunes = UI.sidebar.runesList = {
   cached: {},
   remove: async () => {
-    if (!document.getElementById(AvailableRunes._selected)) return console.error('Runes >> Context Menu: selected item doesn\'t exist !');
+    if (!document.getElementById(AvailableRunes._selected)) return console.error('Available Runes >> Context Menu: selected item doesn\'t exist !');
 
     document.getElementById(AvailableRunes._selected).remove();
     delete AvailableRunes.cached[AvailableRunes._selected];
   },
   transfer: () => {
-    if (!document.getElementById(AvailableRunes._selected)) return console.error('Runes >> Context Menu: selected item doesn\'t exist !');
+    if (!document.getElementById(AvailableRunes._selected)) return console.error('Available Runes >> Context Menu: selected item doesn\'t exist !');
     $('#runesInventory').sortable('option', 'update')(null, { item: $('#' + AvailableRunes._selected), sender: $('#runesInventory') });
   },
   add: page => {
     if (document.getElementById(page._manaMeta.id)) return;
+
+    /* Adding automatically if max hasn't been reached */
+    if (Mana.user.getPerksInventory().getPerks().length < parseInt(Mana.getStore().get('perks-max', 2))) {
+      console.log(`Available Runes >> Adding automatically: ${page.name}`);
+      Mana.user.getPerksInventory().createPerkPage(Mana.helpers.DataValidator.getLeagueReadablePerkPage(page));
+    }
+    else {
+      for (let i = 0; i < parseInt(Mana.getStore().get('perks-max', 2)); i++) {
+        if (Object.values(AvailableRunes.cached).find(x => x.name === Mana.user.getPerksInventory().getPerks()[i].name)) continue;
+        return Mana.user.getPerksInventory().updatePerkPage({ ...page, id: Mana.user.getPerksInventory().getPerks()[i].id });
+      }
+    }
+
     AvailableRunes.cached[page._manaMeta.id] = page;
-    document.getElementById('availableRunes').innerHTML += `<li id="${page._manaMeta.id}" + class="ui-state-default sidebar-button ui-sortable-handle">${page.name}</li>`;
-  },
-  remove: page => {
-    console.error('Runes >> Context Menu: method not working yet !');
+    document.getElementById('availableRunes').innerHTML += `<li id="${page._manaMeta.id}" + class="sidebar-button ui-sortable-handle">${page.name}</li>`;
   }
 };
 
@@ -34,7 +43,7 @@ window.addEventListener('contextmenu', (e) => {
   if (!Mana.user.isLoggedIn) return; /* Security measure in case context menu is still opened when user disconnects */
   AvailableRunes._selected = e.target.id;
 
-  menu.getMenuItemById('transfer').checked = Mana.user.getPerksInventory().getPerks().find(x => x._manaMeta.id == e.target.id);
+  menu.getMenuItemById('transfer').checked = Mana.user.getPerksInventory().getPerks().find(x => x.name == AvailableRunes.cached[e.target.id].name);
   menu.getMenuItemById('transfer').enabled = !menu.getMenuItemById('transfer').checked;
 
   menu.popup({ window: remote.getCurrentWindow() });
@@ -53,9 +62,22 @@ module.exports = {
   inChampionSelect: function(e) {
     document.getElementById('availableRunes').style.display = 'block';
     document.getElementById('availableRunesLabel').style.display = 'block';
+
+    if (!Mana.dev) AvailableRunes.cached = {};
   },
   outChampionSelect: function(e) {
     document.getElementById('availableRunes').style.display = 'none';
     document.getElementById('availableRunesLabel').style.display = 'none';
+
+    Mana.user.getPerksInventory().getPerks().filter(x => Object.values(AvailableRunes.cached).find(y => y.name === x.name)).forEach(z => Mana.user.getPerksInventory().deletePerkPage(z));
+    if (Mana.dev) AvailableRunes.cached = {}; /* Leaves data in memory so we can easily lookup without starting a new game */
+  },
+  championChanged: function(e) {
+    document.getElementById('availableRunes').childNodes.forEach(x => x.remove());
+    AvailableRunes.cached = {};
+  },
+  positionChange: function(e) {
+    let { old, value } = e.detail;
+    console.dir(arguments);
   }
 }
