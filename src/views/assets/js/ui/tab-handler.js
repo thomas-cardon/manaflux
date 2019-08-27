6e5bc08ff02f63e382c6c9864956cfb66c3d5fcd
Thomas <Ryzzzen@users.noreply.github.com>
2019-08-27 21:41:59 +0200

    Tab handling improvements

--- src/views/assets/js/ui/tab-handler.js
+++ src/views/assets/js/ui/tab-handler.js
@@ -1,58 +1,82 @@
 /* Tab Handler */
 const Dots = {
-	isEnabled: document.getElementById('dots') !== null
+	isEnabled: document.getElementById('dots') !== null,
+	onClickEvent: function(dot, tabId, i) {
+		document.querySelectorAll('.tabcontent').forEach(x => x.style.display = 'none');
+		document.querySelector(`.tabcontent[data-tabid="${tabId}"][data-tabn="${i}"]`).style.display = 'block';
+
+		if (!Dots.isEnabled || !dot) return;
+
+		if (Dots.lastSelected)
+			Dots.lastSelected.classList.remove('selected');
+
+		dot.classList.add('selected');
+		Dots.lastSelected = dot;
+
+		Sounds.play('buttonClick');
+	}
 };
 
-Dots.onClickEvent = function(dot, tabId, i) {
-	document.querySelectorAll('.tabcontent').forEach(x => x.style.display = 'none');
-	document.querySelector(`.tabcontent[data-tabid="${tabId}"][data-tabn="${i}"]`).style.display = 'block';
+class Tab extends HTMLElement {
+	constructor() {
+		super();
 
-	if (!Dots.isEnabled || !dot) return;
+		this.style.display = 'none';
 
-	if (Dots.lastSelected)
-		Dots.lastSelected.classList.remove('selected');
+		if (this.getPage() == 0)
+			document.querySelector('button[data-tabid="' + this.getId() + '"]')
+			.addEventListener('click', this.open);
+	}
 
-	dot.classList.add('selected');
-	Dots.lastSelected = dot;
+	getId() {
+		return this.getAttribute('data-tabid');
+	}
 
-	Sounds.play('buttonClick');
+	getPage() {
+		return this.getAttribute('data-tabn');
+	}
+
+	open() {
+		Sounds.play('buttonClick');
+
+		let active = document.querySelector('.tab.active');
+		if (active) active.classList.remove('active');
+
+		event.target.classList.add('active');
+
+		const dotsNumber = document.querySelectorAll(`.tabcontent[data-tabid="${$(event.target).data('tabid')}"]`).length;
+
+		if (dotsNumber === 0) Dots.onClickEvent(null, event.target.getAttribute('data-tabid'), 0);
+		else if (Dots.isEnabled) {
+			let dots = document.getElementById('dots');
+			while (dots.firstChild) dots.removeChild(dots.firstChild);
+
+			if (dotsNumber > 1)
+				for (let i = 0; i < dotsNumber; i++) {
+					const dot = document.createElement('div');
+
+					dot.className = 'dot';
+					dot.onclick = () => Dots.onClickEvent(dot, $(event.target).data('tabid'), i);
+					dots.appendChild(dot);
+				}
+
+			Dots.onClickEvent(dots.firstChild, event.target.getAttribute('data-tabid'), 0);
+		}
+
+		document.getElementById('selected').style.left = event.target.offsetLeft + (event.target.offsetWidth / 2) - (document.getElementById('selected').offsetWidth / 2);
+
+		if (Dots.isEnabled)
+			document.getElementById('dots').style.left = event.target.offsetLeft + (event.target.offsetWidth / 2) - (document.getElementById('dots').offsetWidth / 2);
+	}
 }
 
+window.customElements.define('app-tab', Tab);
+
 document.addEventListener("DOMContentLoaded", function() {
-	document.querySelectorAll('.btn.tab').forEach(tab => {
-		tab.addEventListener('click', event => {
-			Sounds.play('buttonClick');
-
-			document.querySelector('.tab.active').classList.remove('active');
-			event.target.classList.add('active');
-
-			const dotsNumber = document.querySelectorAll(`.tabcontent[data-tabid="${$(event.target).data('tabid')}"]`).length;
-
-			if (dotsNumber === 0) Dots.onClickEvent(null, event.target.getAttribute('data-tabid'), 0);
-			else if (Dots.isEnabled) {
-				let dots = document.getElementById('dots');
-				while (dots.firstChild) dots.removeChild(dots.firstChild);
-
-				if (dotsNumber > 1)
-					for (let i = 0; i < dotsNumber; i++) {
-						const dot = document.createElement('div');
-
-						dot.className = 'dot';
-						dot.onclick = () => Dots.onClickEvent(dot, $(event.target).data('tabid'), i);
-						dots.appendChild(dot);
-					}
-
-				Dots.onClickEvent(dots.firstChild, event.target.getAttribute('data-tabid'), 0);
-			}
-
-			document.getElementById('selected').style.left = event.target.offsetLeft + (event.target.offsetWidth / 2) - (document.getElementById('selected').offsetWidth / 2);
-
-			if (Dots.isEnabled)
-				document.getElementById('dots').style.left = event.target.offsetLeft + (event.target.offsetWidth / 2) - (document.getElementById('dots').offsetWidth / 2);
-		});
-	});
-
 	setTimeout(() => {
-		if (document.querySelector('.btn.tab')) document.querySelector('.btn.tab').click();
+		if (document.querySelector('.btn.tab'))
+			document.querySelector('.btn.tab').click();
+
+		document.getElementById('system-loading').remove();
 	}, 100);
 });
