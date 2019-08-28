074472d8e0cdf02bf1ef498075212fe5d8fbb2b1
Thomas <Ryzzzen@users.noreply.github.com>
2019-08-28 23:16:53 +0200

    perks-max component fixes

--- src/views/assets/js/custom-components/perks-max.js
+++ src/views/assets/js/custom-components/perks-max.js
@@ -1,12 +1,14 @@
 module.exports = {
   load: function(Mana) {
-    Mana.getStore().set('perks-max', this.value = parseInt(Mana.getStore().get('perks-max')));
+    if (!Mana.getStore().get('perks-max') || Mana.getStore().get('perks-max') == 0)
+      Mana.getStore().set('perks-max', 2);
+    else
+      Mana.getStore().set('perks-max', this.value = parseInt(Mana.getStore().get('perks-max')));
   },
   userConnected: async function(e) {
-    this.max = await Mana.user.getPerksInventory().queryCount() || 2;
-    console.log(this.max);
+    this.max = await Mana.user.getPerksInventory().queryCount();
 
-    if (this.value > this.max) {
+    if (this.max && this.value > this.max) {
       Mana.getStore().set('perks-max', parseInt(this.max))
       this.value = this.max;
     }
